605aee93db174db2e0d3c79c487602c2a95f5eb0
Sadek <msadecki@users.noreply.github.com>
2017-04-04 23:25:01 +0200

    Stage 1.7 - little cleanup

--- CSV.Parser/CSV.Parser.Logic.Abstractions/Interfaces/Services/ICachedCsvLineConsumer.cs
+++ CSV.Parser/CSV.Parser.Logic.Abstractions/Interfaces/Services/ICachedCsvLineConsumer.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using CSV.Parser.Logic.Abstractions.Interfaces.Models;
+
 namespace CSV.Parser.Logic.Abstractions.Interfaces.Services
 {
-    public interface ICachedCsvLineConsumer : ICsvLineConsumer, ICsvLinesCache
+    public interface ICachedCsvLineConsumer : ICsvLineConsumer
     {
+        IList<ICsvLine> CsvLines { get; }
     }
 }
--- CSV.Parser/README.md
+++ CSV.Parser/README.md
@@ -1,2 +1,2 @@
 # CSV.Parser
-It is CSV parser that should parse CSV files compatible with the standard for the CSV format defined by RFC 4180 [https://tools.ietf.org/html/rfc4180].
+It is CSV parser that should parse CSV files compatible with the standard for the CSV format defined by [RFC 4180](https://tools.ietf.org/html/rfc4180).
